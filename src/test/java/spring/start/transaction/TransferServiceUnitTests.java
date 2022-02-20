85871eb207e4da2a7a7a04114f0c15dc5a0645bc
handrian87 <96159013+handrian87@users.noreply.github.com>
2022-02-19 21:03:32 -0500

    Testing transfer method using @Mock-@InjectMocks-@ExtendWith annotation

--- src/test/java/spring/start/transaction/TransferServiceUnitTests.java
+++ src/test/java/spring/start/transaction/TransferServiceUnitTests.java
@@ -2,6 +2,10 @@ package spring.start.transaction;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import spring.start.transaction.Model.Account;
 import spring.start.transaction.Repo.AccountRepository;
 import spring.start.transaction.Service.TransferService;
@@ -14,14 +18,48 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-
+@ExtendWith(MockitoExtension.class)
 public class TransferServiceUnitTests {
-    @Test
+    //region test-transferMoney v1
+    /*@Test
     @DisplayName("Test the amount is transferred from one account to another account" +
             "if no exception occurs.")
     public void moneyTransferHappyFlow(){
         AccountRepository accountRepository = mock(AccountRepository.class);
         TransferService transferService = new TransferService(accountRepository);
+        Account sender = new Account();
+        sender.setId(1);
+        sender.setAmount(new BigDecimal(1000));
+
+        Account destination = new Account();
+        destination.setId(2);
+        destination.setAmount(new BigDecimal(1000));
+
+        given(accountRepository.findAccountById(sender.getId()))
+                .willReturn(sender);
+        given(accountRepository.findAccountById(destination.getId()))
+                .willReturn(destination);
+
+        transferService.transferMoney(
+                sender.getId(),
+                destination.getId(),
+                new BigDecimal(100)
+        );
+        verify(accountRepository)
+                .changeAmount(1, new BigDecimal(900));
+        verify(accountRepository)
+                .changeAmount(2, new BigDecimal(1100));
+    }*/
+    //endregion
+
+    @Mock
+    private AccountRepository accountRepository;
+    @InjectMocks
+    private TransferService transferService;
+
+    @Test
+    public void moneyTransferHappyFlow(){
+
         Account sender = new Account();
         sender.setId(1);
         sender.setAmount(new BigDecimal(1000));
